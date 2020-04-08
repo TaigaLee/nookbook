e2930ccc88b18a7cb3b13fab2f77fde111424694
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-08 17:28:45 -0400

    add some styling to user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
 
-router.get('/', (req, res) => {
-    res.render('user/index.ejs')
+router.get('/', async (req, res, next) => {
+  if (req.session.loggedIn) {
+    const currentUser = await User.findById(req.session.userId).populate('island');
+    res.render('user/index.ejs', {
+        user: currentUser
+    })
+  } else {
+    res.redirect('/auth/login');
+  }
 })
 
+// add profile picture router 
 router.get('/add-profile', (req, res) => {
+  if (req.session.loggedIn) {
     res.render('user/add-profile.ejs')
+  } else {
+    res.redirect('/auth/login')
+  }
 })
+
+router.post('/add-profile', (req, res) => {
+  res.send('after add profile picture action')
+})
+
 
 module.exports = router
