f13744d7ad50acef89df4bb8b7b092cb3858bbac
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-08 17:58:31 -0400

    different user show page rendering

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,9 @@ const User = require('../models/user')
 router.get('/', async (req, res, next) => {
   if (req.session.loggedIn) {
     const currentUser = await User.findById(req.session.userId).populate('island');
+    req.session.notcurrentUser = false;
     res.render('user/index.ejs', {
-        user: currentUser
+      user: currentUser
     })
   } else {
     res.redirect('/auth/login');
@@ -24,6 +25,20 @@ router.get('/add-profile', (req, res) => {
 
 router.post('/add-profile', (req, res) => {
   res.send('after add profile picture action')
+})
+
+// show other account profile
+router.get('/:username', async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username: req.params.username });
+        const notcurrentUser = req.session.username !== req.params.username ? true : false;
+        res.render('user/index.ejs', {
+            user: user,
+            notcurrentUser: notcurrentUser
+        })
+    } catch (err) {
+        next(err);
+    }
 })
 
 
--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 	res.locals.message = req.session.message
 	res.locals.username = req.session.username
 	res.locals.loggedIn = req.session.loggedIn
+  res.locals.notcurrentUser = req.session.notcurrentUser
 	next()
 })
 
