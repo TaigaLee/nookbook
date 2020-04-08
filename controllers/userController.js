f64b551ddbddec0a78de080b2a423c6613758b05
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-08 16:56:31 -0400

    add profile picture pseudo code

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('user')
+    res.render('user/index.ejs')
+})
+
+router.get('/add-profile', (req, res) => {
+    res.render('user/add-profile.ejs')
 })
 
 module.exports = router
