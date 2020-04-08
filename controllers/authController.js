43fbe135933bb2cf281fa6d6047668888ddb461a
Taiga Lee <taiga@taigalee.com>
2020-04-08 10:52:42 -0500

    starting post route for registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
   res.render("auth/register.ejs");
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const desiredUsername = req.body.username;
+    const desiredPassword = req.body.password;
+    const islandName = req.body.island;
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
